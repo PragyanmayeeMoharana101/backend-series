bc45f0b7a0a8ca3eb8826ce381c4887b3bb727a6
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-12-24 16:39:29 +0530

    creating routes in diffrent folder

---
+++ DB-Connection/24-12/db.js
@@ -0,0 +1,11 @@
+const mongoose = require ("mongoose");
+const connectDB = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("database connected sucessfully")
+    } catch (error) {
+        console.log("database connection failed");
+        process.exit(0);
+    }
+}
+module.exports = connectDB
