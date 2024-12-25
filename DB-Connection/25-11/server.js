87d88af6e80c25736a49a7e4f2bb9151eeed0f82
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-12-25 22:22:34 +0530

    finding data by id from database

---
+++ DB-Connection/25-11/server.js
@@ -0,0 +1,12 @@
+const dotenv = require ("dotenv");
+dotenv.config();
+const express = require ("express");
+const connectDB = require("./db");
+const getallbooks = require("./controllers/bookcontroller");
+const app = express();
+connectDB();
+app.use ("/books/api", require ("./routes/bookRoutes"))
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`)
+})
