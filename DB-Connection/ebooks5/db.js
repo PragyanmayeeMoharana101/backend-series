fca952666e94b95d3c796be699759b33393fd2b1
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2025-01-06 01:23:56 +0530

    connecting database

---
+++ DB-Connection/ebooks5/db.js
@@ -0,0 +1,11 @@
+const mongoose = require ("mongoose")
+const connectDB = async()=>{
+   try {
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log("my database connected sucessfully")
+   } catch (error) {
+    console.log("database connection lost")
+    process.exit(0)
+   }
+}
+module.exports = connectDB;
