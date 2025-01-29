4e56f01f0331f3d1f93f29f0f3b1e84c548dbba7
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2025-01-29 22:06:05 +0530

    creating routes in separate file for retrieving data for database

--- DB-Connection/24-12/routes/bookrouter.js
+++
@@ -1,8 +0,0 @@
-const express = require ("express");
-const { getAllBooks, getBookById } = require("../controllers/bookController");
-
-const router = express.Router();
-router.get("/getAll",getAllBooks);
-router.get("/findbook/:id",getBookById);
-
-module.exports = router
