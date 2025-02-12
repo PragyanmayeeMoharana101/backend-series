4f801f6b705556c23c4582334273cc47670c1970
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2025-02-12 23:56:59 +0530

    creating routes in separate file for getting data by id from database

---
+++ DB-Connection/26-12/controllers/bookcontroller.js
@@ -0,0 +1,75 @@
+const ebooks = require ("../model/ebooks")
+const getallbooks = async (req,res)=>{
+    try {
+        let allBooks = await ebooks.find({});
+        res.json({
+            message: "list of all books",
+            success: "true",
+            bookDetails: allBooks
+        })
+    } catch (error) {
+        res.json({
+            message: "data not found",
+            success: "false",
+            bookDetails: allBooks
+        })
+    }
+}
+const getBookByID = async (req,res)=>{
+    try {
+        const bookID =  req.params.id;
+        console.log(bookID);
+        let allbooks = await ebooks.findById(bookID)
+        res.json({
+            message: "book details",
+            success: "true",
+            data: allbooks      
+        })
+        
+    } catch (error) {
+        res.json({
+            message: "no data found",
+            success: "false",     
+        })
+    }
+    
+}
+
+const getBookByChoice = async (req,res)=>{
+    try {
+        const bookID =  req.params.id;
+        console.log(bookID);
+        let allbooks = await ebooks.find({id: bookID})
+        res.json({
+            message: "book details",
+            success: "true",
+            data: allbooks      
+        })
+        
+    } catch (error) {
+        res.json({
+            message: "no data found",
+            success: "false",     
+        })
+    }
+    
+}
+
+const addbook = async (req,res)=>{
+    try {
+      const newbook = req.body;
+      console.log(newbook)
+      res.json({
+        message: "data added sucessfully",
+        success: "true",
+        data : newbook
+      })  
+    } catch (error) {
+        res.json({
+            message: "no data found",
+            success: "false",     
+        }) 
+    }
+
+}
+module.exports = {getallbooks, getBookByID,getBookByChoice}
